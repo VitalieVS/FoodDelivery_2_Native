c44c803ec870a70cc15acc150eedb0e0c261fd75
VitalieVSV <dut66AHJCT>
2019-11-29 17:52:36 +0200

    GIT -1

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#theTarget").skippr({
+        transition: 'slide',
+        speed: 1000,
+        easing: 'easeOutQuart',
+        navType: 'block',
+        childrenElementType: 'div',
+        arrows: true,
+        autoPlay: false,
+        autoPlayDuration: 5000,
+        keyboardOnAlways: true,
+        hidePrevious: false
+
+    });
+});
