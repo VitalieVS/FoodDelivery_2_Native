100532ff9661f853cc7d4fa0eba5be9b74055150
VitalieVSV <dut66AHJCT>
2019-12-01 18:45:28 +0200

    GIT -1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,18 @@
 $(document).ready(function(){
-    $("#theTarget").skippr({
-        transition: 'slide',
-        speed: 1000,
-        easing: 'easeOutQuart',
-        navType: 'block',
-        childrenElementType: 'div',
-        arrows: true,
-        autoPlay: false,
-        autoPlayDuration: 5000,
-        keyboardOnAlways: true,
-        hidePrevious: false
 
+    $("#carouselExampleIndicators").swiperight(function() {
+        $(this).carousel('prev');
     });
+
+    $("#carouselExampleIndicators").swipeleft(function() {
+        $(this).carousel('next');
+    });
+
+    $(".item-ico").click(function () {
+            $( "#barb" ).dialog();
+    })
+
+    $( function() {
+        $( "#dialog" ).dialog();
+    } );
 });
