24bbd4de4b64086aa9d350eeec926d14096dfaf2
VitalieVSV <dut66AHJCT>
2019-12-03 21:24:41 +0200

    GIT

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,77 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    let cartPrice = [];
+    let cartCount = 0;
+    let totalPriceItems = 0;
 
-    $("#carouselExampleIndicators").swiperight(function() {
-        $(this).carousel('prev');
+    $(function () {
+        $(document).tooltip();
     });
 
-    $("#carouselExampleIndicators").swipeleft(function() {
-        $(this).carousel('next');
+    $('.item-price').click(function () {
+        let itemPrice = ($(this).attr("data-value") * 1);
+        priceAdd(itemPrice);
+        cartDisplay();
     });
 
-    $(".item-ico").click(function () {
-            $( "#barb" ).dialog();
-    })
+    function incrementCartItems() {
+        cartCount++;
+    }
 
-    $( function() {
-        $( "#dialog" ).dialog();
-    } );
+    function cartDisplay() {
+        document.getElementById("cart-count").innerHTML = cartCount;
+        document.getElementById("total-price").innerHTML = totalPriceItems;
+    }
+
+    function priceAdd(itemPrice) {
+        cartPrice.push(itemPrice);
+        cartSum();
+        priceClear();
+    }
+
+    function priceClear() {
+        cartPrice = [];
+    }
+
+    function cartSum() {
+        cartPrice.forEach(totalPrice);
+        incrementCartItems();
+    }
+
+    function totalPrice(item) {
+        totalPriceItems += item;
+    }
+
+    function usaShow(){
+        document.getElementById("rest-ul").children[0].style.display="block";
+        document.getElementById("rest-ul").children[1].style.display="block";
+        document.getElementById("rest-ul").children[3].style.display="block";
+    }
+
+    function usaHide(){
+        document.getElementById("rest-ul").children[0].style.display="none";
+        document.getElementById("rest-ul").children[1].style.display="none";
+        document.getElementById("rest-ul").children[3].style.display="none";
+    }
+
+    $('.button-usa').click(function () {
+        usaShow();
+        belgiumHide();
+    });
+
+    $('.button-belgium').click(function(){
+        usaHide();
+        belgiumShow();
+    });
+
+    function belgiumShow() {
+        document.getElementById("rest-ul").children[2].style.display="block";
+        document.getElementById("rest-ul").children[4].style.display="block";
+        document.getElementById("rest-ul").children[5].style.display="block";
+    }
+    function belgiumHide(){
+        document.getElementById("rest-ul").children[2].style.display="none";
+        document.getElementById("rest-ul").children[4].style.display="none";
+        document.getElementById("rest-ul").children[5].style.display="none";
+    }
+
 });
